3ad9718f0afe997da303eeffb2d1b67e16c8801f
Andrii Isiuk <andrii.isiuk@skygate.io>
2020-04-18 17:29:40 +0200

    Fixed sending attachments with emails.

--- core/views.py
+++ core/views.py
@@ -1,8 +1,7 @@
 from django.db import transaction
-from rest_framework import viewsets, status
-from rest_framework.response import Response
+from rest_framework import viewsets
 
-from core.models import Email, Attachment
+from core.models import Email
 from core.serializers import EmailSerializer, EmailCreateSerializer
 from core.services.mails import send_email
 
@@ -18,13 +17,7 @@ class EmailsViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         with transaction.atomic():
             mail = serializer.save(files=self.request.FILES)
-        print(
-            55555555555555555555555555555555555555555555555555557777, mail.attachments
-        )
 
-        mail = Email.objects.last()
-        attachments = Attachment.objects.last()
-        print(66666666666666, mail, "|||", attachments.attachment)
         send_email(mail)
 
     # TODO - send all emails
