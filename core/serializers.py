2533a4cd77a151966e88321705519d14f8cd2515
Andrii Isiuk <andrii.isiuk@skygate.io>
2020-04-18 14:43:44 +0200

    Added viewset to create and show emails with attachments.

--- core/serializers.py
+++ core/serializers.py
@@ -3,7 +3,30 @@ from rest_framework import serializers
 from .models import Attachment, Email
 
 
+class AttachmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attachment
+        fields = ("attachment",)
+
+
 class EmailSerializer(serializers.ModelSerializer):
+    attachments = AttachmentSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Email
+        fields = (
+            "id",
+            "sender",
+            "recipients",
+            "title",
+            "message",
+            "status",
+            "priority",
+            "attachments",
+        )
+
+
+class EmailCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Email
         fields = (
@@ -16,8 +39,15 @@ class EmailSerializer(serializers.ModelSerializer):
             "priority",
         )
 
+    def create(self, validated_data):
+        files = validated_data["files"]
+        del validated_data["files"]
 
-class AttachmentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Attachment
-        fields = ("id", "attachment", "email")
+        email = Email.objects.create(**validated_data)
+
+        attachments = []
+        for attachment in files:
+            attachments.append(Attachment(attachment=attachment, email=email))
+        Attachment.objects.bulk_create(attachments)
+
+        return email
