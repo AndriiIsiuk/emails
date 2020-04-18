7b5524c5770ac7d7a9aeba5d7148cd5ea08be7d8
Andrii Isiuk <andrii.isiuk@skygate.io>
2020-04-18 18:49:01 +0200

    Made an endpoint, which allows to send all pending mails.

--- core/tasks.py
+++ core/tasks.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from core.models import Email
 from core.services.mails import send_email
 from emails.celery_app import app
@@ -8,3 +10,13 @@ def celery_send_created_email(email_pk: int) -> None:
     """Sending new email task with SENT status"""
     email = Email.objects.get(pk=email_pk)
     send_email(email)
+
+
+@app.task
+def celery_send_all_pending(pending_emails: List[int]) -> None:
+    """Sending all emails with PENDING status"""
+    for pk in pending_emails:
+        email = Email.objects.get(pk=pk)
+        send_email(email)
+        email.status = Email.SENT
+        email.save()
--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
 from django.db import transaction
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from core.models import Email
 from core.serializers import EmailSerializer, EmailCreateSerializer
-from core.tasks import celery_send_created_email
+from core.tasks import celery_send_created_email, celery_send_all_pending
 
 
 class EmailsViewSet(viewsets.ModelViewSet):
@@ -21,5 +23,10 @@ class EmailsViewSet(viewsets.ModelViewSet):
         if mail.status == Email.SENT:
             celery_send_created_email.delay(mail.pk)
 
-    # TODO - send all emails
-    # TODO - select_related("attachments")
+    @action(detail=False, methods=["POST"])
+    def send_all_pending(self, request):
+        pending = Email.objects.filter(status=Email.PENDING).values_list(
+            "pk", flat=True
+        )
+        celery_send_all_pending.delay(list(pending))
+        return Response(status=status.HTTP_204_NO_CONTENT)
