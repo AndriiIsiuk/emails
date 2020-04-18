ff2ee9c0281e02efd81adb4f8b9055e172766c3b
Andrii Isiuk <andrii.isiuk@skygate.io>
2020-04-18 23:18:30 +0200

    Add tests for  emails list.

--- core/tests/factories/email.py
+++ core/tests/factories/email.py
@@ -1,15 +1,29 @@
+import string
+from random import choice
+from typing import List
+
 import factory
 
 from core.models import Email
+from factory.fuzzy import FuzzyText, FuzzyChoice
+
+
+def email_generator(resolver, size: int = 6, chars=string.ascii_lowercase) -> str:
+    name = "".join(choice(chars) for _ in range(size))
+    return f"{name}@example.org"
+
+
+def email_list_generator(resolver, size: int = 3) -> List[str]:
+    return [email_generator(resolver) for _ in range(size)]
 
 
 class EmailFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Email
 
-    sender = factory.LazyAttribute(lambda o: "%s@example.org" % o)
-    recipients = factory.LazyAttribute(lambda o: "%s@example.org" % o)
-    title = factory.fuzzy.FuzzyText(length=32)
-    message = factory.fuzzy.FuzzyText(length=64)
-    status = factory.fuzzy.FuzzyChoice(Email.STATUSES, getter=lambda s: s[0])
-    priority = factory.fuzzy.FuzzyChoice(Email.PRIORITY, getter=lambda p: p[0])
+    sender = factory.LazyAttribute(email_generator)
+    recipients = factory.LazyAttribute(email_list_generator)
+    title = FuzzyText(length=32)
+    message = FuzzyText(length=64)
+    status = FuzzyChoice(Email.STATUSES, getter=lambda s: s[0])
+    priority = FuzzyChoice(Email.PRIORITY, getter=lambda p: p[0])
