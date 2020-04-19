8a73977c9a8db65535fb9861689beb5fa0ec088d
Andrii Isiuk <andrii.isiuk@skygate.io>
2020-04-19 13:46:50 +0200

    Add tests for sending emails functions.

---
+++ core/tests/tasks/test_sending_mails.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+
+from core.models import Email
+from core.tasks import celery_send_all_pending
+from core.tests.factories.email import EmailFactory
+
+
+class TestCelerySendTasks(TestCase):
+    def setUp(self):
+        self.email = EmailFactory()
+
+    def test_send_emails_pending(self):
+        for _ in range(2):
+            EmailFactory(status=Email.PENDING)
+
+        pending = Email.objects.filter(status=Email.PENDING).values_list(
+            "pk", flat=True
+        )
+        self.assertEqual(len(pending), 3)
+
+        celery_send_all_pending(pending)
+
+        self.assertEqual(Email.objects.filter(status=Email.SENT).count(), 3)
+        self.assertEqual(Email.objects.filter(status=Email.PENDING).count(), 0)
--- core/tests/views/test_emails_viewset.py
+++ core/tests/views/test_emails_viewset.py
@@ -1,18 +1,91 @@
 from django.urls import reverse
+from rest_framework import status
 from rest_framework.test import APITestCase
 
+from core.models import Email
 from core.tests.factories.email import EmailFactory
+
+
+def _fake_celery_send_all_pending(ids):
+    return True
 
 
 class TestItemsApiView(APITestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.item_url = reverse("core:email-list")
+        cls.emails_list_url = reverse("core:email-list")
+        cls.emails_send_pending_url = reverse("core:email-send-all-pending")
 
     def setUp(self):
         self.email = EmailFactory()
+        self.email2_data = {
+            "sender": "test1@send.it",
+            "recipients": ["one@one.com", "two@two.com"],
+            "title": "Testing mail",
+            "message": "Test content",
+        }
 
     def test_emails_list(self):
-        response = self.client.get(self.item_url)
-        print(response.json())
+        response = self.client.get(self.emails_list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.json()["results"]), 1)
+
+    def test_emails_create(self):
+        email_data = self.email2_data
+        email_data["status"] = Email.SENT
+
+        emails = Email.objects.all().count()
+        self.assertEqual(emails, 1)
+        response = self.client.post(self.emails_list_url, email_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        emails = Email.objects.all().count()
+        self.assertEqual(emails, 2)
+        last = Email.objects.all().last()
+        self.assertEqual(last.status, Email.SENT)
+
+    def test_emails_create_default_status(self):
+        response = self.client.post(self.emails_list_url, self.email2_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        last = Email.objects.all().last()
+        self.assertEqual(last.status, Email.PENDING)
+
+    def test_email_details(self):
+        response = self.client.post(self.emails_list_url, self.email2_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        emails_detail_url = reverse("core:email-detail", args=(response.json()["id"],))
+
+        response_detail = self.client.get(emails_detail_url)
+        self.assertEqual(response_detail.status_code, status.HTTP_200_OK)
+
+        email = response_detail.json()
+        self.assertEqual(email["sender"], self.email2_data["sender"])
+        self.assertEqual(email["title"], self.email2_data["title"])
+        self.assertEqual(email["message"], self.email2_data["message"])
+        self.assertEqual(email["status"], Email.PENDING)
+        self.assertEqual(email["priority"], Email.LOW)
+
+        self.assertListEqual(
+            sorted(email["recipients"]), sorted(self.email2_data["recipients"])
+        )
+        self.assertCountEqual(email["recipients"], self.email2_data["recipients"])
+
+    def test_emails_create_without_duplicated_recipients(self):
+        email_with_duplicated_recipients = self.email2_data
+        email_with_duplicated_recipients["recipients"] = [
+            "one@one.com",
+            "two@two.com",
+            "two@two.com",
+            "two@two.com",
+            "two@two.com",
+        ]
+
+        response = self.client.post(
+            self.emails_list_url, email_with_duplicated_recipients
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        last = Email.objects.all().last()
+        self.assertEqual(sorted(last.recipients), ["one@one.com", "two@two.com"])
